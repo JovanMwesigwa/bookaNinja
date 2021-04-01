2b45bf66453f6b3234d1d34de06b6d0ae7d7dbf6
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2021-04-01 15:47:46 +0300

    Fiexed bugs

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,30 +1,30 @@
 import React from 'react'
 
-export const getStaticPaths = async() => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await res.json();
+// export const getStaticPaths = async() => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await res.json();
 
-    const paths = data.map(ninja => {
-        return {
-            params: {id: ninja.id.toString()}
-        }
-    })
+//     const paths = data.map(ninja => {
+//         return {
+//             params: {id: ninja.id.toString()}
+//         }
+//     })
 
-    return {
-        paths,
-        fallBack: false
-    }
-}
+//     return {
+//         paths,
+//         fallBack: false
+//     }
+// }
 
-export const getStaticProps = async (context) => {
-    const id = context.params.id
-    const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id)
-    const data = await res.json();
+// export const getStaticProps = async (context) => {
+//     const id = context.params.id
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id)
+//     const data = await res.json();
 
-    return {
-        props: {ninja: data}
-    }
-}
+//     return {
+//         props: {ninja: data}
+//     }
+// }
 
 export default function NinjaDetails({ ninja }) {
     return (
