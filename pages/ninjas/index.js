3b4b65dc340ee50a090ea82331a4cacdca9f97fd
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2021-05-02 14:07:48 +0300

    Added ninjas listing

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import Head from 'next/head'
+import styles from '../../styles/Ninjas.module.css'
 
-function NinjaList() {
+export const getStaticProps = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+}
+
+function NinjaList({ ninjas }) {
     return (
         <>
             <Head>
@@ -9,6 +19,13 @@ function NinjaList() {
             </Head>
             <div>
                 <h1>Ninja Listing</h1>
+                {
+                    ninjas.map(ninja => (
+                        <div key={ninja.id} className={styles.single}>
+                            <h3>{ ninja.name }</h3>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
